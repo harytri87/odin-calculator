bcaf6b23ece6b5c2aa17b625b2828468a29ec830
harytri87 <harytri87@gmail.com>
2025-10-08 19:15:25 +0700

    refactor: basic math operations
    Changed them now to make it easier to add functionality to the UI later
    
    Also:
    - Switched top and bottom parts within the history
    - Changed some font colors to be lighter

--- script.js
+++ script.js
@@ -1,45 +1,18 @@
-const add = function (a, b) {
-  return a + b;
+const method = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => a / b,
+};
+
+const operate = function (a, operator, b) {
+  a = Number(a);
+  b = Number(b);
+
+  return method[operator](a, b);
 }
 
-const subtract = function (a, b) {
-  return a - b;
-}
-
-const multiply = function (a, b) {
-  return a * b;
-}
-
-const divide = function (a, b) {
-  return a / b;
-}
-
-const operate = function (input) {
-  let split = input.split(" ");
-  let a = Number(split[0]);
-  let operator = split[1];
-  let b = Number(split[2]);
-
-  switch (operator) {
-    case "+":
-      return add(a, b);
-      break;
-
-    case "-":
-      return subtract(a, b);
-      break;
-      
-    case "*":
-      return multiply(a, b);
-      break;
-      
-    case "/":
-      return divide(a, b);
-      break;
-  }
-}
-
-console.log("add 6 and 2 = ", operate("6 + 2"));
-console.log("subtract 6 and 2 = ", operate("6 - 2"));
-console.log("multiply 6 and 2 = ", operate("6 * 2"));
-console.log("divide 6 and 2 = ", operate("6 / 2"));
+console.log("add 6 and 2 = ", operate(6, "+", 2));
+console.log("subtract 6 and 2 = ", operate(6, "-", 2));
+console.log("multiply 6 and 2 = ", operate(6, "*", 2));
+console.log("divide 6 and 2 = ", operate(6, "/", 2));
